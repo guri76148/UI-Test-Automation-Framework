3ee47985a26eab3331374df0dd0a1d9d0cafdfd4
Gurkaran <gurkaran7575@gmail.com>
2026-04-15 17:27:04 -0400

    Merge branch 'master' of https://github.com/guri76148/Test-Automation-Framework.git

--- src/test/java/com/ui/tests/LoginTestOld.java
+++ src/test/java/com/ui/tests/LoginTestOld.java
@@ -11,27 +11,30 @@ import com.utility.BrowserUtility;
 public class LoginTestOld {
 
 	public static void main(String[] args) {
-		WebDriver wd = new ChromeDriver(); // Launch Browser Window !! Browser Session is created!!
-		
-		BrowserUtility browserUtility = new BrowserUtility(wd);
-		browserUtility.goToWebsite("https://automationpractice.techwithjatin.com/");
-		browserUtility.maximizeWindow();
-		
-		
-		By signInLinkLocator = By.xpath("//a[contains(text(), \"Sign in\")]");
-		browserUtility.clickOn(signInLinkLocator);
-		
-		
-		By emailTextBoxLocator =  By.id("email");
-		browserUtility.enterText(emailTextBoxLocator, "gofew20353@marvetos.com");
-		
-		
-		By passwordTextBoxLocator =  By.id("passwd");
-		browserUtility.enterText(passwordTextBoxLocator, "password");
-		
-		
-		By submitLoginButtonLocator =  By.id("SubmitLogin");
-		browserUtility.clickOn(submitLoginButtonLocator);
+		/*
+		 * WebDriver wd = new ChromeDriver(); // Launch Browser Window !! Browser
+		 * Session is created!!
+		 * 
+		 * BrowserUtility browserUtility = new BrowserUtility(wd);
+		 * browserUtility.goToWebsite("https://automationpractice.techwithjatin.com/");
+		 * browserUtility.maximizeWindow();
+		 * 
+		 * 
+		 * By signInLinkLocator = By.xpath("//a[contains(text(), \"Sign in\")]");
+		 * browserUtility.clickOn(signInLinkLocator);
+		 * 
+		 * 
+		 * By emailTextBoxLocator = By.id("email");
+		 * browserUtility.enterText(emailTextBoxLocator, "gofew20353@marvetos.com");
+		 * 
+		 * 
+		 * By passwordTextBoxLocator = By.id("passwd");
+		 * browserUtility.enterText(passwordTextBoxLocator, "password");
+		 * 
+		 * 
+		 * By submitLoginButtonLocator = By.id("SubmitLogin");
+		 * browserUtility.clickOn(submitLoginButtonLocator);
+		 */
 	}
 
 }
